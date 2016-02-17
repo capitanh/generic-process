5f44c6c23a7381d0d7c85a3e65370e79064e7f67
Luis Novo <luis.novo@gmail.com>
2016-02-17 23:13:13 +0100

    Some minor changes

--- src/main/java/net/mikasa/genericprocess/MainProcess.java
+++ src/main/java/net/mikasa/genericprocess/MainProcess.java
@@ -14,14 +14,14 @@ import org.joda.time.Period;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class MainProcess 
+public class MainProcess
 {
 	static final Logger log = LogManager.getLogger(MainProcess.class.getName());
 	private static ApplicationContext context;
-	
+
 	public static void main(String[] args)
 	{
-		
+
 		Date start = new Date();
 		//Load Spring context
 		context = new ClassPathXmlApplicationContext("classpath:applicationContext.xml","classpath:net/sf/appinfo/appInfoDefaultContext.xml");
@@ -31,15 +31,15 @@ public class MainProcess
 		log.info("# Version: " + appInfoText + " #");
 		log.info("# Process start date: " + start.toString() + "                                                        #");
 		log.info("###########################################################################################################");
-		
+
 		//Main Process
 		log.info("Main process start");
-		
+
 		try
 		{
 			log.info("Reading configuration file:");
 			Configuration config = new PropertiesConfiguration("config.properties");
-			
+
 			Map<String,String> mProperties = new HashMap<String, String>();
 			Iterator<String> it = config.getKeys();
 			while(it.hasNext())
@@ -47,7 +47,7 @@ public class MainProcess
 				String key = it.next();
 				String value = config.getString(key);
 				mProperties.put(key, value);
-				log.info("Loaded " + key + " = " + value);
+				log.info("  Loaded " + key + " = " + value);
 			}
 			log.info("Read " + mProperties.size() + " configuration properties");
 		}
@@ -55,15 +55,15 @@ public class MainProcess
 		{
 			log.error("Main process error: " + e);
 		}
-		
+
 		Date end = new Date();
 		Period period = new Period(start.getTime(), end.getTime());
 		log.info("################################################################################");
-		log.info("# Process result: OK                                                           #");		
+		log.info("# Process result: OK                                                           #");
 		log.info("# Time elapsed: " +  String.format("%02d:%02d:%02d",period.getHours(), period.getMinutes(), period.getSeconds()) + "                                                       #");
-		log.info("################################################################################");		
+		log.info("################################################################################");
 		log.info("All done");
-		System.exit(0);		
-		
+		System.exit(0);
+
 	}
 }
